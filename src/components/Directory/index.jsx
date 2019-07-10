976c53f5358c62268851631f996089a4b9d0698d
rayGreycloud <rayGreycloud@gmail.com>
2019-07-09 17:33:04 -0700

    add menu bkg imgs & styles

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -4,21 +4,24 @@ import MenuItem from '../Menu-item';
 import { directoryData } from './directory.data.js';
 
 class Directory extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      sections: [...directoryData]
-    };
-  }
+  //   this.state = {
+  //     sections: [...directoryData]
+  //   };
+  // }
+  state = {
+    sections: [...directoryData]
+  };
 
   render() {
     const { sections } = this.state;
 
     return (
       <div className='directory-menu'>
-        {sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        {sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
--- src/components/Menu-item/index.jsx
+++ src/components/Menu-item/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className='menu-item'>
+    <div className={`menu-item ${size}`}>
+      <div
+        className='background-image'
+        style={{ background: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
