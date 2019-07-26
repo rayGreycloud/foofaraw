d06101c803304c3e7048087c01436d80d5873d38
rayGreycloud <rayGreycloud@gmail.com>
2019-07-26 10:53:21 -0700

    Add Header, Logo and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import HomePage from './pages/HomePage/';
 import ShopPage from './pages/ShopPage';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
