2517c631065ca2418c8bb58ce10949ee3e4e3d9a
rayGreycloud <rayGreycloud@gmail.com>
2019-08-13 09:10:51 -0700

    add redux and store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import './styles/style.scss';
 import App from './App';
+
+import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/style.scss';
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
