d2947227efc719ee86b35ad0426404bf993ea490
rayGreycloud <rayGreycloud@gmail.com>
2019-08-14 11:44:45 -0700

    Add toggle to cart icon to hide cart dropdown

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ import CartDropdown from '../CartDropdown';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, isHidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -31,12 +31,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {!isHidden && <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { isHidden } }) => ({
+  currentUser,
+  isHidden
 });
 
 export default connect(mapStateToProps)(Header);
