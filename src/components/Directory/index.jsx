29067140d56160af5bcc02a47ff2a7f4d7122448
rayGreycloud <rayGreycloud@gmail.com>
2019-08-19 12:37:27 -0700

    Refactor directory reducer; add selector

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../MenuItem';
 
@@ -11,8 +14,8 @@ const Directory = ({ sections }) => (
   </div>
 );
 
-const mapStateToProps = ({ directory }) => ({
-  sections: [...directory]
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
