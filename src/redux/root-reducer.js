135d31463f342ba0bcb2a2af23bcdd3d77d6b71b
rayGreycloud <rayGreycloud@gmail.com>
2019-08-19 11:57:31 -0700

    Add redux-persist; implement store persistence

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   cart: cartReducer,
-  user: userReducer
+  user: userReducer,
+  directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
