2838ed51ef0a38a912a6a989f06ab11276a0e79f
rayGreycloud <rayGreycloud@gmail.com>
2019-08-20 06:37:25 -0700

    style correction

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
-
 import MenuItem from '../MenuItem';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 const Directory = ({ sections }) => (
   <div className='directory-menu'>
