38e46f2bef9ddc5f874c64efc29576dd5a8a966a
rayGreycloud <rayGreycloud@gmail.com>
2019-08-20 12:17:52 -0700

    Refactor preview

--- src/components/CollectionPreview/index.jsx
+++ src/components/CollectionPreview/index.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 import CollectionItem from '../CollectionItem';
 
 const CollectionPreview = ({ title, items }) => {
+  const itemsToDisplay = items
+    .filter((item, idx) => idx < 4)
+    .map(item => <CollectionItem key={item.id} item={item} />);
+
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items
-          .filter((item, idx) => idx < 4)
-          .map(item => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
+      <div className='preview'>{itemsToDisplay}</div>
     </div>
   );
 };
