0c95ba408d2dfea62b88bea8fa366188a7f83ac4
rayGreycloud <rayGreycloud@gmail.com>
2019-08-20 08:26:21 -0700

    add shop selector for collection

---
+++ src/redux/constants.js
@@ -0,0 +1,7 @@
+export const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,19 @@
 import { createSelector } from 'reselect';
 
+import { COLLECTION_ID_MAP } from '../constants';
+
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectShopCollection = collectionUrlParam =>
+  createSelector(
+    [selectShopCollections],
+    collections =>
+      collections.find(
+        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+      )
+  );
