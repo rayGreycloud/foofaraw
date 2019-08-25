1c7f80aeeb7aca3b9bfcb083c9e0c58b032aa7ac
rayGreycloud <rayGreycloud@gmail.com>
2019-08-25 12:48:41 -0700

    Convert checkout page to styled component

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -5,6 +5,11 @@ import { createStructuredSelector } from 'reselect';
 import CheckoutHeader from '../../components/CheckoutHeader';
 import CheckoutItem from '../../components/CheckoutItem';
 import StripeCheckoutButton from '../../components/StripeCheckoutButton';
+import {
+  StyledCheckoutPage,
+  StyledCheckoutItems,
+  StyledTestWarning
+} from './styled.checkout-page';
 
 import {
   selectCartItems,
@@ -12,24 +17,24 @@ import {
 } from '../../redux/cart/cart.selectors';
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
+  <StyledCheckoutPage>
     <CheckoutHeader />
-    <div className='checkout-items'>
+    <StyledCheckoutItems>
       {cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} item={cartItem} />
       ))}
-    </div>
-    <div className='checkout-total'>
+    </StyledCheckoutItems>
+    <div className='total'>
       <span>TOTAL ${total}</span>
     </div>
 
     <StripeCheckoutButton price={total} />
-    <div className='test-warning'>
+    <StyledTestWarning>
       *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </div>
-  </div>
+    </StyledTestWarning>
+  </StyledCheckoutPage>
 );
 
 const mapStateToProps = createStructuredSelector({
