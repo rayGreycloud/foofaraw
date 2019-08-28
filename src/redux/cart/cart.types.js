a1ce0f1e191975c4e6d5f3cee4c4cddcc32866ad
rayGreycloud <rayGreycloud@gmail.com>
2019-08-28 10:50:13 -0700

    Move action types

---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,4 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const DECREASE_ITEM_QTY = 'DECREASE_ITEM_QTY';
+export const TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN';
--- src/redux/constants.js
+++
@@ -1,7 +0,0 @@
-export const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
