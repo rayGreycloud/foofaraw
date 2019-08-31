c4028ded236af519dd83e74b79c1c71873a3bb15
rayGreycloud <rayGreycloud@gmail.com>
2019-08-31 09:44:21 -0700

    Clean out commented code

--- src/App.js
+++ src/App.js
@@ -14,16 +14,11 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import { setCurrentUser } from './redux/user/user.actions';
 import { GlobalStyle } from './styled.global-style';
 
-// Setting up firebase collections db
-// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
-// import { addCollectionAndDocuments } from './firebase/firebase.utils';
-
 class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    // const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -38,12 +33,6 @@ class App extends Component {
       }
 
       setCurrentUser(userAuth);
-
-      // load shop data from firebase
-      // addCollectionAndDocuments(
-      //   'collections',
-      //   collectionsArray.map(({ title, items }) => ({ title, items }))
-      // );
     });
   }
 
@@ -75,8 +64,6 @@ class App extends Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-  // currentUser: selectCurrentUser,
-  // collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
